43ae7f4bcae0147270b4409b0c40ce4bafffa1e0
Simpcyclassy <chioma.onyekpere@gmail.com>
2023-09-17 15:31:54 +0200

    format the tree count

--- app/main.py
+++ app/main.py
@@ -28,7 +28,9 @@ class HTTPRequestHandler(BaseHTTPRequestHandler):
     def get_treecounter(self):
         self.do_HEAD()
         tree_count = fetch_tree_count()
-        bytes_template = bytes(html_template.substitute(counter=tree_count), "utf-8")
+        # Format tree_count with commas
+        formatted_tree_count = '{:,}'.format(tree_count)
+        bytes_template = bytes(html_template.substitute(counter=formatted_tree_count), "utf-8")
         self.wfile.write(bytes_template)
 
     def do_HEAD(self):
